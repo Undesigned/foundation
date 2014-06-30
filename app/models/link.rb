9331a2774a9575a33516d7eae0db0f1d6ab0d06d
Neil Gupta <neil@tabuleapp.com>
2014-06-29 23:10:45 -0500

    Add index on links table, stylize link titles

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 class Link < ActiveRecord::Base
   attr_accessible :href, :title
   belongs_to :owner, polymorphic: true
+
+  # Return stylized titles 
+  def title
+    t = read_attribute(:title)
+    return case t
+    when 'angellist' then 'AngelList'
+    when 'linkedin' then 'LinkedIn'
+    when 'aboutme' then 'About.me'
+    when 'github' then 'GitHub'
+    else t.titleize
+    end
+  end
 end
--- db/migrate/20140629003242_create_links.rb
+++ db/migrate/20140629003242_create_links.rb
@@ -1,11 +1,13 @@
 class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
-      t.string :href
       t.string :title
+      t.string :href
       t.string :owner_type
       t.integer :owner_id
       t.timestamps
     end
+
+    add_index :links, [:owner_id, :owner_type, :title]
   end
 end
